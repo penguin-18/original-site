b123cdb6b3798f5461dfc7ad14aed0e0e99d37af
clrnt <otsuka_m@beartail.jp>
2019-10-07 11:07:34 +0900

    駅名が空欄でも保存できるように

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,10 +2,10 @@ class Restaurant < ApplicationRecord
   validates :name, presence: true, length: { maximum: 255 }
   validates :url, presence: true, length: { maximum: 255 }
   validates :image_url, presence: true, length: { maximum: 255 }
-  validates :station, presence: true, length: { maximum: 255 }
+  validates :station, length: { maximum: 255, allow_blank: true }
   validates :category, presence: true, length: { maximum: 255 }
   validates :code, presence: true, length: { maximum: 255 }
-  
+
   has_many :visits
   has_many :users, through: :visits
 end
